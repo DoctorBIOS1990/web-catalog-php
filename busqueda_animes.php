d0c8af3740555c5ff88b22fda5ef8a7fac24fca3
Jorge Benito Ugás <147073922+DoctorBIOS1990@users.noreply.github.com>
2024-12-28 02:26:15 +0100

    Add lastest 8 animes

--- busqueda_animes.php
+++ busqueda_animes.php
@@ -3,6 +3,8 @@
 	$contador = 0;
 	$consultaAnime = new Consulta($baseAnime, 'ANIMES');
 
+	$ultimosALL = $consultaAnime->fetchAllRecords("SELECT ani_id, ani_img FROM ANIMES ORDER by ani_id DESC LIMIT 8");
+
 	function getRecomendados($consultaAnime){
 		$aux =  $consultaAnime->getAllRecords("SELECT ani_id, ani_img FROM ANIMES WHERE recomendado NOTNULL and ani_id > :id Order by ani_id LIMIT 8;");
 		while (!$aux) $aux = getRecomendados($consultaAnime);
@@ -44,7 +46,6 @@
 			padding-bottom: 0;
 		}
 	</style>
-
 </head>
 <body>
 
@@ -70,6 +71,7 @@
 	
 </section>
 
+<!---Portadas de Busquedas-->
 <section style="padding-top:0;padding-bottom:0;" class="portfolio" id="sectionSearch">
 	<div class="disponible-content">
 		<?php if ($_POST) foreach($sentencia as $anime){ ?>
@@ -104,6 +106,30 @@
 		else if ($sentencia == false) echo '<style>#sectionSearch{background:none;}</style>
 		<div class="main-text"><h3 style="text-align:center;">"No se encuentra."</h3></div>'; 
 	?>
+</section>
+
+<!---ULTIMOS Section-->
+<section class="portfolio" id="portfolio" style="padding-bottom:0;">
+
+    <div class="main-text">
+       <h2 style="margin-top:30px;"><span>Los</span>Recientes<span>.</span></h2>
+       <p><svg xmlns="" width="128" height="128" fill="currentColor" class="bi bi-calendar-date" viewBox="0 0 16 16">
+       <path d="M6.445 11.688V6.354h-.633A13 13 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23"/>
+       <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
+       </svg></p>
+       <h3 style="padding-left:50px;padding-right:50px;">No se pierda las ultimas actualizaciones de la sede <strong>Imperio Animes</strong>, </br>aqui mostramos los ocho agregados recientemente.</h3>
+    </div>
+
+    <div class="ultimos-content">
+    <center>
+	    </br></br></br> 
+        <?php foreach($ultimosALL as $u){ ?>
+            <a href="info_anime.php?id=<?php echo $u->ani_id ?>">
+                <img src="data:image/jpeg;base64,<?php echo base64_encode($u->ani_img) ?>" />
+             </a>
+        <?php }?>
+    </center>
+    </div>
 </section>
 
 <!---Recomendados Animes Section-->
