9b406c3e68eff4f892949b9182d3ed23d4f9a781
Jorge Benito Ugás <147073922+DoctorBIOS1990@users.noreply.github.com>
2024-12-31 16:10:29 +0100

    Modified css

--- components/marquee.php
+++ components/marquee.php
@@ -1,20 +1,20 @@
 <center>
 <div style="width:70%; bottom:0;">
     <marquee behavior="scroll" direction="left" scrollamount="10">
-        <img src="assets/images/companys/1.png" alt="Imagen 1">  
-        <img src="assets/images/companys/2.png" alt="Imagen 1">  
-        <img src="assets/images/companys/3.png" alt="Imagen 1">  
-        <img src="assets/images/companys/4.png" alt="Imagen 1">  
-        <img src="assets/images/companys/5.png" alt="Imagen 1">  
-        <img src="assets/images/companys/6.png" alt="Imagen 1">  
-        <img src="assets/images/companys/7.png" alt="Imagen 1">  
-        <img src="assets/images/companys/8.png" alt="Imagen 1">  
-        <img src="assets/images/companys/9.png" alt="Imagen 1">  
-        <img src="assets/images/companys/10.png" alt="Imagen 1">  
-        <img src="assets/images/companys/11.png" alt="Imagen 1">  
-        <img src="assets/images/companys/12.png" alt="Imagen 1">  
-        <img src="assets/images/companys/13.png" alt="Imagen 1">  
-        <img src="assets/images/companys/14.png" alt="Imagen 1">  
+        <img src="assets/images/companys/1.png" alt="Company 1">  
+        <img src="assets/images/companys/2.png" alt="Company 2">  
+        <img src="assets/images/companys/3.png" alt="Company 3">  
+        <img src="assets/images/companys/4.png" alt="Company 4">  
+        <img src="assets/images/companys/5.png" alt="Company 5">  
+        <img src="assets/images/companys/6.png" alt="Company 6">  
+        <img src="assets/images/companys/7.png" alt="Company 7">  
+        <img src="assets/images/companys/8.png" alt="Company 8">  
+        <img src="assets/images/companys/9.png" alt="Company 9">  
+        <img src="assets/images/companys/10.png" alt="Company 10">  
+        <img src="assets/images/companys/11.png" alt="Company 11">  
+        <img src="assets/images/companys/12.png" alt="Company 12">  
+        <img src="assets/images/companys/13.png" alt="Company 13">  
+        <img src="assets/images/companys/14.png" alt="Company 14">  
     </marquee>
 </div>
 </center>
